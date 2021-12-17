485425b30a6dedcea02e5de8c8bec11688e5476d
Guillaume <guiwagne@gmail.com>
2021-12-17 08:47:57 +0100

    inc modif

--- inc/layout.php
+++ inc/layout.php
@@ -1,14 +1,9 @@
-<?php
-    session_start()
-?>  
 <!DOCTYPE HTML>
 <html lang="fr">
     <head>
         <meta charset="UTF-8"/>
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
         <title>Simpléduc</title>
-        <!-- CSS only -->
-        
         <link rel="stylesheet" href="./css/style.css"/>
     </head>
     <body>
