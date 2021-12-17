cb19afc60c326f8e6097d59d8f63d21d4c8a6181
Guillaume <guiwagne@gmail.com>
2021-12-17 10:37:41 +0100

    hhh

--- accueil.php
+++ accueil.php
@@ -4,20 +4,11 @@
         <title>Accueil | Simpléduc</title>
     </head>
     <body>
-<?php 
+<?php
     include("./inc/header.php");
 ?>
 
-<div class="header_bar">
-    <nav class="nav">
-        <ul class="nav_ul">
-            <a href="projets"><li class="arial">projets</li></a>
-            <a href="test"><li class="arial">test</li></a>
-        </ul>
-    </nav>
-</div>
-
-<h1>Accueil seuleument pour ceux authentifier !</h1>
+        <h1>Accueil seuleument pour ceux authentifier !</h1>
 
 
 <?php 
--- inc/header.php
+++ inc/header.php
@@ -1,3 +1,11 @@
-        <div>
-            <h1 class="arial">Simpléduc</h1>
+        <div class="header_index">
+            <div class="info_right">
+                <div class="header_right">
+                    <a href="projets"><li class="arial">projets</li></a>
+                    <a href="test"><li class="arial">test</li></a>
+                </div>
+            </div>
+            <div>
+                <h1 class="arial">Simpléduc</h1>
+            </div>
         </div>
