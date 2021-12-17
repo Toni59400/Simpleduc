b833a3c4df2d39a0a7cb90b3f547ab2a1a22edea
Guillaume <guiwagne@gmail.com>
2021-12-17 08:41:13 +0100

    inc

---
+++ inc/layout.php
@@ -0,0 +1,14 @@
+<?php
+    session_start()
+?>  
+<!DOCTYPE HTML>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <title>Simpléduc</title>
+        <!-- CSS only -->
+        
+        <link rel="stylesheet" href="./css/style.css"/>
+    </head>
+    <body>
