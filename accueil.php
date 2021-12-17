08d7cddcaa10ce75cac382367d25d795f9e91f2b
Guillaume <guiwagne@gmail.com>
2021-12-17 09:16:23 +0100

    rien

--- accueil.php
+++ accueil.php
@@ -2,5 +2,13 @@
     include("./inc/layout.php");
 ?>
 
+<div class="header_bar">
+    <nav class="nav">
+        <ul class="nav_ul">
+            <a href="projets"><li class="arial">projets</li></a>
+            <a href="test"><li class="arial">test</li></a>
+        </ul>
+    </nav>
+</div>
 
 <h1>Accueil seuleument pour ceux authentifier !</h1>
